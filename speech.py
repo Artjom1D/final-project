b4dca421d577a463ea9b696226657466a6b1a08d
Artjom1D <153429637+Artjom1D@users.noreply.github.com>
2024-07-14 13:26:17 +0300

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+
+def speech_to_text():
+    mic = sr.Microphone()
+    recog = sr.Recognizer()
+
+    with mic as audio_file:
+        recog.adjust_for_ambient_noise(audio_file)
+        audio = recog.listen(audio_file)
+        return recog.recognize_google(audio, language="ru-RU")
+
+
